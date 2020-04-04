9bd726da1021e951bf31ab3b1f09110b332ba0b4
Luis Ferreira <luispaulo_26@hotmail.com>
2020-04-04 20:23:28 +0100

    TestCase and TestSuite created

---
+++ Test Suites/FeaturesTestSuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>FeaturesTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b7a17d48-4aeb-46b3-9fcf-b4dc9df50963</testSuiteGuid>
+   <testCaseLink>
+      <guid>ec3569a9-170c-496f-bded-cca44f2969e8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseFeatureXpto</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
